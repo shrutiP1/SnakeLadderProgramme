fb1ab696858355798abc04f7c654ee298125c46c
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-05 22:11:52 +0530

    Die is rolled to get digit

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,6 +2,11 @@ package com.bridgeLabz.snackladder;
 
 public class SnakeLadder
 {
+	public int getRollNum()
+    {
+   	  int dieRollNum=(int)(Math.floor(Math.random()*10)%6)+1;
+	  return dieRollNum;
+    }
 	public static void main(String[] args)
 	{
 		int startPos=0;	
@@ -9,7 +14,11 @@ public class SnakeLadder
 		//UC1
 		
 		System.out.println("startPosition of user is "+startPos);
+		//UC2
 		
+		SnakeLadder s1=new SnakeLadder();
+		int digit=s1.getRollNum();
+		System.out.println("After rolling die number is  "+digit);
 	}
 
 }
