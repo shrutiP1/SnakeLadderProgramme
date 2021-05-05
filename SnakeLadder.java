0ea55a74cebb8480c20283785fadee4d968b8d79
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-05 22:00:36 +0530

    Welcome

---
+++ SnakeLadder.java
@@ -0,0 +1,11 @@
+package com.bridgeLabz.snackladder;
+
+public class SnakeLadder
+{
+	public static void main(String[] args)
+	{
+		System.out.println("welcome to snackladder programme");
+		
+	}
+
+}
