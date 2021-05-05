346a5afeb97a626d7d861c13767ad3dad722a5bc
shrutiP1 <shrutipatil13798@gmail.com>
2021-05-05 23:56:31 +0530

    Add: Play with 2 player and check who won

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -52,6 +52,53 @@ public class SnakeLadder
 
     	return startPos;
     }
+	public int getOptionTwoUser(int dieRollNum , SnakeLadder s)
+    {
+		//System.out.println(startPos);
+		int optionNo=(int)Math.floor(Math.random()*10)%3;
+		switch (optionNo)
+		{
+		   case NO_PLAY:
+			   break;
+			   
+		   case LADDER:
+			   startPos=startPos+dieRollNum;
+			   if(startPos>100)
+			   {
+			      startPos=startPos-dieRollNum;
+			      
+			   }
+			   else if(startPos==100)
+			   {
+				   startPos=100;
+				   break;
+			   }
+			   
+			     s.getPositionTwoUser(s);
+				   break;
+			   
+			  
+			case SNAKE:
+		     	   startPos=startPos-dieRollNum;
+		 	        break;
+    }
+		System.out.println(startPos);	
+	    return startPos;
+   }
+	public int getPositionTwoUser(SnakeLadder s1)
+    {
+    	int num=0;
+    	num=s1.getRollNum();
+    	 startPos=s1.getOptionTwoUser(num , s1);
+    	 //System.out.println(startPos);
+    	 if(startPos<0)
+    	 {
+    		startPos=0;
+    	 }
+    	
+    	return startPos;
+    	
+    }
 	public static void main(String[] args)
 	{
 		int count=0;
@@ -89,7 +136,35 @@ public class SnakeLadder
             //UC6
 
 	    System.out.println("dice was played "+count+" no of time to win");
-
+         //UC7
+	    
+	    SnakeLadder player1=new SnakeLadder();
+	    SnakeLadder player2=new SnakeLadder();
+	    int startPos1=0;
+	    int startPos2=0;
+	    while(startPos1<=100 && startPos2<=100)
+	    {
+	    	startPos1=player1.getOptionTwoUser(startPos2, player1);
+	    	startPos2=player2.getOptionTwoUser(startPos2, player2);
+	    	
+	    	if(startPos1==100)
+	    	{
+	    		System.out.println("player 1 win");
+	    		break;
+	    		
+	    	}
+	    	else if(startPos2==100)
+	    	{
+	    		System.out.println("player 2 win");
+	    		break;
+	    		
+	    	}
+	    	else
+	    	{
+	    		startPos1=player1.getOptionTwoUser(startPos2, player1);
+		    	startPos2=player2.getOptionTwoUser(startPos2, player2);
+	    	}
+	    }
 	}
 
 }
